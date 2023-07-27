1a68c06b213f7c1aae842d433fb5a315ebbfa621
Ziqi Wang <86229718+Lizard-LZD@users.noreply.github.com>
2023-07-27 16:20:05 +0100

    try redux

---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../Styles/Home.css';
+import { connect } from 'react-redux';
+import { increment, decrement, incrementByAmount } from '../Redux/Reducers/Reducer1'; // Import your action creators here
+
+// Define mapStateToProps function
+const mapStateToProps = (state) => {
+  return {
+    count: state.counter.count, // Bind counter slice's count state to the component's props
+  };
+};
+
+// Define mapDispatchToProps function (using object syntax)
+const mapDispatchToProps = {
+  increment,          // Bind the increment action creator to the component's props
+  decrement,          // Bind the decrement action creator to the component's props
+  incrementByAmount,  // Bind the incrementByAmount action creator to the component's props
+};
+
+// Define a React component
+function MyComponent(props) {
+  return (
+    <div>
+      <p>Count: {props.count}</p>
+      <button onClick={props.increment}>Increment</button>
+      <button onClick={props.decrement}>Decrement</button>
+      <button onClick={() => props.incrementByAmount(5)}>Increment by 5</button>
+    </div>
+  );
+}
+
+// Use connect to wrap the component and pass both mapStateToProps and mapDispatchToProps
+export default connect(mapStateToProps, mapDispatchToProps)(MyComponent);
+
+
--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -5,5 +5,8 @@ import counterSlice from "./Reducers/Reducer1"
 export default configureStore({
   reducer: {
    counter : counterSlice
+   //Dominate State
+   //UI State
+   //APP Loading
   },
 });
